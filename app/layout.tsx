cb99d1c7624e975cc287fce4f28793283d8a4e90
OmiAtTexas <opm281706@gmail.com>
2026-05-24 13:12:16 -0500

    add favicon.ico fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" href="/tab-logo.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/tab-logo.png" />
       </head>
       <body>
         <SessionProvider>
