e85b40bf324eb6ccdeda70054cf266bbe3897566
OmiAtTexas <opm281706@gmail.com>
2026-05-24 10:11:48 -0500

    change font to Lora serif

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/svg+xml" href="/Users/ommore/Desktop/My Mac/Esti AI/Esti AI/public/Estimated_sign.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
       </head>
       <body>
         <SessionProvider>
