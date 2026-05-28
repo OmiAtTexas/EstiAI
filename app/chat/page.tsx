82b250480bf78fb5616fe71e323d67fe838683be
OmiAtTexas <opm281706@gmail.com>
2026-05-28 11:17:09 -0500

    fix ClientRedirect prerender error

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react"
 import { ChatWindow } from "@/components/chat/ChatWindow"
 import { ClientRedirect } from "./ClientRedirect"
 
 export default function NewChat() {
   return (
     <>
-      <ClientRedirect />
+      <Suspense fallback={null}>
+        <ClientRedirect />
+      </Suspense>
       <ChatWindow chatId={null} messages={[]} />
     </>
   )
